a72e4162e230f08ea702ab6eb41a18314a8b3c6f
Afr Schoe <58883403+q9f@users.noreply.github.com>
2022-06-02 19:07:17 +0200

    lib: implement announce binding

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,23 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn announce(binding: &str) {
+    alert(&format!("Loaded drand-wasm target for {}", binding));
+}
+
+
 #[cfg(test)]
 mod tests {
+    use crate::announce;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    #[should_panic(expected = "cannot call wasm-bindgen imported functions on non-wasm targets")]
+    fn it_loads_bindings() {
+        assert_eq!(announce("foo"), ());
     }
 }
